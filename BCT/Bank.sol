1ec978509cc309a33626f7abdfdbe0dc6bd575af
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-11-10 12:53:25 +0530

    Update Bank.sol

--- BCT/Bank.sol
+++ BCT/Bank.sol
@@ -1,1 +1,30 @@
+pragma solidity ^0.8.0;
 
+contract Bank{
+
+    address public accHolder;
+    uint256 balance = 0;
+
+    constructor(){
+        accHolder = msg.sender;
+    }
+
+    function withdraw() public payable{
+        require(msg.sender == accHolder, "you are not the account owner.");
+        require(balance > 0,"Deposit amount should be greater than 0.");
+        balance += msg.value;
+    
+    }
+
+    
+    function deposit() public payable {
+        require(msg.sender == accHolder, "You are not the account owner."); 
+        require(msg.value > 0, "Deposit amount should be greater than 0."); 
+        balance += msg.value; 
+    }
+
+    function ShowBlance() public view returns (uint256){
+        require(msg.sender == accHolder,"you are not the account owner.");
+        return balance;
+    } 
+}
